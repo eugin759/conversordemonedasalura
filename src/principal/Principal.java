0cff2b6b4852997cb03f54e90d877e5affe153b7
Eugenio Bricio <eugeniobricio759@gmail.com>
2024-05-14 15:49:40 -0600

    Revision 2, subiendo cambios a traves de intelliJ

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -6,5 +6,16 @@ public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+
+        while (true){
+            System.out.println("*************************************************************************");
+            System.out.println("Welcome to the los pollos hermanos family");
+            System.out.println();
+
+
+
+        }
+
     }
 }
